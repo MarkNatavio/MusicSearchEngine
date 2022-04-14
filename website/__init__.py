c1691a5180b44eebec5bd1f0ec51d05cc27761be
Mark Natavio <43160780+MarkNatavio@users.noreply.github.com>
2022-04-14 04:47:00 -0400

    New Features
    Added navigation bar, bootstrap, and created login, logout, home, and signup pages

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ml;kpCFGHVJHBNwezgdhxgfUIGYTRw32jchv,jhbk;iopooi0p9o8i67ru5e4y">:LPKJUIYT'
     
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
     return app
